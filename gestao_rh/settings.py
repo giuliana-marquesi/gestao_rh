5b311133508ce808caabaeca487ba5bd1ae4f8c1
giuliana-marquesi <giugianam@gmail.com>
2018-11-18 14:45:20 -0200

    criado o app funcionario

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'empresa.apps.EmpresaConfig',
+    'funcionario.apps.FuncionarioConfig',
 ]
 
 MIDDLEWARE = [
