753cf6098d94dd5167cb9c0a80e15bea19ca997e
giuliana-marquesi <giugianam@gmail.com>
2018-11-18 15:31:30 -0200

    criado o app departamento

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'empresa.apps.EmpresaConfig',
     'funcionario.apps.FuncionarioConfig',
+    'departamento.apps.DepartamentoConfig',
 ]
 
 MIDDLEWARE = [
